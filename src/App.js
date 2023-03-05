56ee391fb277b9b677178cc6497ec0000335ec25
username <Ryan>
2023-03-04 21:39:19 -0600

    update

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
         <div className="container">
           <div className="text-center p-4 )">
             <input
-              className="rounded-2xl p-3 text-lg  border-solid  bg-gray-900 bg-[rgba(0,0,0,0.25)] outline-none"
+              className="rounded-2xl p-3 text-lg  border-solid  bg-gray-900  ] outline-none"
               type="text"
               value={location}
               onChange={(event) => setLocation(event.target.value)}
